388bfd8e7c9f04b9c2185b8c44d4ffd6bee92632
Lazzaro <lcarlucci@deloitte.it>
2026-03-17 16:54:09 +0100

    Davinciflow 0.1

--- react-samples-master/Ping/Sample-01/src/components/Highlight.js
+++ react-samples-master/Ping/Sample-01/src/components/Highlight.js
@@ -4,8 +4,6 @@ import PropTypes from "prop-types";
 import hljs from "highlight.js";
 import "highlight.js/styles/monokai-sublime.css";
 
-const registeredLanguages = {};
-
 class Highlight extends Component {
   constructor(props) {
     super(props);
@@ -15,22 +13,7 @@ class Highlight extends Component {
   }
 
   componentDidMount() {
-    const { language } = this.props;
-
-    if (language && !registeredLanguages[language]) {
-      try {
-        const newLanguage = require(`highlight.js/lib/languages/${language}`);
-        hljs.registerLanguage(language, newLanguage);
-        registeredLanguages[language] = true;
-
-        this.setState({ loaded: true }, this.highlight);
-      } catch (e) {
-        console.error(e);
-        throw Error(`Cannot register the language ${language}`);
-      }
-    } else {
-      this.setState({ loaded: true });
-    }
+    this.setState({ loaded: true }, this.highlight);
   }
 
   componentDidUpdate() {
@@ -40,7 +23,7 @@ class Highlight extends Component {
   highlight = () => {
     this.codeNode &&
       this.codeNode.current &&
-      hljs.highlightBlock(this.codeNode.current);
+      hljs.highlightElement(this.codeNode.current);
   };
 
   render() {
